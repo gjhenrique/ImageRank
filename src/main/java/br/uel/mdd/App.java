fabdb3d6a569191f320cf3260d4728ca19356443
Guilherme Henrique <gjhenrique@gmail.com>
2014-05-20 13:18:16 -0300

    Esturtura inicial do projeto

---
+++ src/main/java/br/uel/mdd/App.java
@@ -0,0 +1,13 @@
+package br.uel.mdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

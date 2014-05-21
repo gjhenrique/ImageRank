16cac54dbfd03b5d804a446de3700c503d92c286
Pedro Tanaka <pedro.stanaka@gmail.com>
2014-05-21 13:19:34 -0300

    Adicionado classe para leitura de arquivos DICOM's

--- src/main/java/br/uel/io/DicomFileOpener.java
+++ src/main/java/br/uel/io/DicomFileOpener.java
@@ -2,15 +2,34 @@ package br.uel.io;
 
 import ij.IJ;
 import ij.ImagePlus;
+import ij.io.Opener;
 
 /**
  * Example class that opens DICOM files.
  * @author Pedro Tanaka <pedro.stanaka@gmail.com>
  */
 public class DicomFileOpener {
+
+    public ImagePlus getImage() {
+        return image;
+    }
+
+    private ImagePlus image;
+
     public DicomFileOpener(String filePath){
-        ImagePlus imagePlus = null;
-        imagePlus = (ImagePlus) IJ.runPlugIn("ij.plugin.DICOM", filePath);
-        System.out.println("Dimensions: " + imagePlus.getNDimensions());
+        image = (ImagePlus) IJ.runPlugIn("ij.plugin.DICOM", filePath);
+    }
+
+    public double[][] getGrayPixelMatrix(){
+        double[][] matrix = null;
+        if(image != null && image.getType() == Opener.DICOM){
+            matrix = new double[image.getWidth()][image.getHeight()];
+            for (int i = 0; i < image.getWidth(); i++) {
+                for (int j = 0; j < image.getHeight(); j++) {
+                    matrix[i][j] = (double) image.getPixel(i, j)[0];
+                }
+            }
+        }
+        return matrix;
     }
 }
--- src/main/java/br/uel/mdd/App.java
+++ src/main/java/br/uel/mdd/App.java
@@ -3,6 +3,8 @@ package br.uel.mdd;
 import br.uel.io.DicomFileOpener;
 import math.jwave.exceptions.JWaveFailure;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -16,5 +18,8 @@ public class App
     public App(){
         String filePath = this.getClass().getResource("/pulmao_enfisema.dcm").getPath();
         DicomFileOpener dicomOpener = new DicomFileOpener(filePath);
+
+        // Get the gray value of a random pixel
+        System.out.println(Arrays.toString(dicomOpener.getImage().getPixel(1, 20)));
     }
 }

652bb0b82ce0a3ef2b7c54e50a134ea90d8af914
Guilherme Henrique <gjhenrique@gmail.com>
2014-05-30 16:13:32 -0300

    Adicionando migrations alterando o esquema da tabela extractions

--- sql/schema_migrations.sql
+++ sql/schema_migrations.sql
@@ -4,3 +4,21 @@
  -- Date/Time: <date>
  */
 
+-- Author: Guilherme
+-- Date/Time: 30/05/2014 15:59
+ALTER TABLE extractors DROP COLUMN family;
+ALTER TABLE extractors DROP COLUMN support;
+ALTER TABLE extractors ADD COLUMN filter_identifier VARCHAR(50);
+ALTER TABLE extractors ADD COLUMN class_name VARCHAR(50);
+ALTER TABLE extractors ADD COLUMN levels_wavelet INT;
+
+-- Author: Guilherme
+-- Date/Time: 30/05/2014 15:59
+INSERT INTO extractors (id, name, filter_identifier, class_name)
+  VALUES (1, 'Wavelet SubEspaco', 'Haar1', 'ReducedScaleWaveletExtractor');
+
+INSERT INTO extractors (id, name, filter_identifier, class_name)
+  VALUES (2, 'Wavelet SubEspaco', 'Daubechies2', 'ReducedScaleWaveletExtractor');
+
+INSERT INTO extractors (id, name, filter_identifier, class_name)
+  VALUES (3, 'Wavelet SubEspaco', 'Symlets2', 'ReducedScaleWaveletExtractor')

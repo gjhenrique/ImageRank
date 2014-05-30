26180e5dfc8f67b4f280b71ad62c201dd6e024e8
Pedro Tanaka <pedro.stanaka@gmail.com>
2014-05-29 22:05:44 -0300

    Adicionado arquivo de migrações
    Arquivo para controlar alterações ao banco

---
+++ sql/schema_migrations.sql
@@ -0,0 +1,10 @@
+/**
+ Format:
+ -- Author: <name>
+ -- Date/Time: <date> <time>
+ */
+
+-- Author: Pedro Tanaka
+-- Date/Time:
+ALTER TABLE classes
+    ADD COLUMN tipo_imagem VARCHAR(255) NOT NULL DEFAULT 'none';

f8873debe26c7b93b429d69086d1ae394d3285f8
Guilherme Henrique <gjhenrique@gmail.com>
2014-05-31 15:51:19 -0300

    Novas migracoes no esquema

--- sql/schema_migrations.sql
+++ sql/schema_migrations.sql
@@ -4,7 +4,6 @@
  -- Date/Time: <date>
  */
 
-
 -- Pedro
 -- 30/05/14
 ALTER TABLE dataset_classes
@@ -15,3 +14,29 @@ ALTER TABLE images
     ADD COLUMN dataset_class_id INT,
     ADD CONSTRAINT fk_images_dataset_classes FOREIGN KEY (dataset_class_id)
       REFERENCES dataset_classes(id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+-- Author: Guilherme
+-- Date/Time: 30/05/2014 15:59
+ALTER TABLE extractors DROP COLUMN family;
+
+ALTER TABLE extractors DROP COLUMN support;
+
+ALTER TABLE extractors ADD COLUMN filter_identifier VARCHAR ( 50 );
+ALTER TABLE extractors ADD COLUMN class_name VARCHAR ( 50 );
+ALTER TABLE extractors ADD COLUMN levels_wavelet INT;
+
+-- Author: Guilherme
+-- Date/Time: 30/05/2014 15:59
+INSERT INTO extractors (id, name, filter_identifier, class_name, levels_wavelet)
+VALUES (1, 'Wavelet SubEspaco', 'Haar1', 'ReducedScaleWaveletExtractor', 4);
+
+INSERT INTO extractors (id, name, filter_identifier, class_name, levels_wavelet)
+VALUES (2, 'Wavelet SubEspaco', 'Daubechies2', 'ReducedScaleWaveletExtractor', 4);
+
+INSERT INTO extractors (id, name, filter_identifier, class_name, levels_wavelet)
+VALUES (3, 'Wavelet SubEspaco', 'Symlets2', 'ReducedScaleWaveletExtractor', 4);
+
+-- Author: Guilherme
+-- Date/Time: 31/05/2014 12:55
+ALTER TABLE extractions DROP COLUMN extraction_data;
+ALTER TABLE extractions ADD COLUMN extraction_data DOUBLE PRECISION [];

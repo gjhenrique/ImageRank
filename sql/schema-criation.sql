aac23c02aacb21386da6ea9dfc6b578c6a9e01cf
Pedro Tanaka <pedro.stanaka@gmail.com>
2014-06-01 18:27:53 -0300

    Correções na criação do esquema

--- sql/schema-criation.sql
+++ sql/schema-criation.sql
@@ -1,183 +1,74 @@
 CREATE TABLE class_image
 (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255)
 );
-CREATE TABLE class_image_id_seq
-(
-    sequence_name VARCHAR NOT NULL,
-    last_value BIGINT NOT NULL,
-    start_value BIGINT NOT NULL,
-    increment_by BIGINT NOT NULL,
-    max_value BIGINT NOT NULL,
-    min_value BIGINT NOT NULL,
-    cache_value BIGINT NOT NULL,
-    log_cnt BIGINT NOT NULL,
-    is_cycled BOOL NOT NULL,
-    is_called BOOL NOT NULL
-);
+
 CREATE TABLE dataset_classes
 (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     class_id INT,
     dataset_id INT
 );
-CREATE TABLE dataset_classes_id_seq
-(
-    sequence_name VARCHAR NOT NULL,
-    last_value BIGINT NOT NULL,
-    start_value BIGINT NOT NULL,
-    increment_by BIGINT NOT NULL,
-    max_value BIGINT NOT NULL,
-    min_value BIGINT NOT NULL,
-    cache_value BIGINT NOT NULL,
-    log_cnt BIGINT NOT NULL,
-    is_cycled BOOL NOT NULL,
-    is_called BOOL NOT NULL
-);
+
 CREATE TABLE datasets
 (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255)
 );
-CREATE TABLE datasets_id_seq
-(
-    sequence_name VARCHAR NOT NULL,
-    last_value BIGINT NOT NULL,
-    start_value BIGINT NOT NULL,
-    increment_by BIGINT NOT NULL,
-    max_value BIGINT NOT NULL,
-    min_value BIGINT NOT NULL,
-    cache_value BIGINT NOT NULL,
-    log_cnt BIGINT NOT NULL,
-    is_cycled BOOL NOT NULL,
-    is_called BOOL NOT NULL
-);
+
 CREATE TABLE extractions
 (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     image_id INT NOT NULL,
     extractor_id INT,
     extraction_data _FLOAT8,
     extraction_time BIGINT
 );
-CREATE TABLE extractions_id_seq
-(
-    sequence_name VARCHAR NOT NULL,
-    last_value BIGINT NOT NULL,
-    start_value BIGINT NOT NULL,
-    increment_by BIGINT NOT NULL,
-    max_value BIGINT NOT NULL,
-    min_value BIGINT NOT NULL,
-    cache_value BIGINT NOT NULL,
-    log_cnt BIGINT NOT NULL,
-    is_cycled BOOL NOT NULL,
-    is_called BOOL NOT NULL
-);
+
 CREATE TABLE extractors
 (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(50),
     filter_identifier VARCHAR(50),
     class_name VARCHAR(50),
     levels_wavelet INT
 );
-CREATE TABLE extractors_id_seq
-(
-    sequence_name VARCHAR NOT NULL,
-    last_value BIGINT NOT NULL,
-    start_value BIGINT NOT NULL,
-    increment_by BIGINT NOT NULL,
-    max_value BIGINT NOT NULL,
-    min_value BIGINT NOT NULL,
-    cache_value BIGINT NOT NULL,
-    log_cnt BIGINT NOT NULL,
-    is_cycled BOOL NOT NULL,
-    is_called BOOL NOT NULL
-);
+
 CREATE TABLE images
 (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     file_name VARCHAR(255),
     image BYTEA,
     dataset_class_id INT,
-    pixel_matrix _INT4
+    mime_type VARCHAR(60)
 );
-CREATE TABLE images_id_seq
-(
-    sequence_name VARCHAR NOT NULL,
-    last_value BIGINT NOT NULL,
-    start_value BIGINT NOT NULL,
-    increment_by BIGINT NOT NULL,
-    max_value BIGINT NOT NULL,
-    min_value BIGINT NOT NULL,
-    cache_value BIGINT NOT NULL,
-    log_cnt BIGINT NOT NULL,
-    is_cycled BOOL NOT NULL,
-    is_called BOOL NOT NULL
-);
+
 CREATE TABLE metric_evaluator
 (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     class_name VARCHAR(255) NOT NULL
 );
-CREATE TABLE metric_evaluator_id_seq
-(
-    sequence_name VARCHAR NOT NULL,
-    last_value BIGINT NOT NULL,
-    start_value BIGINT NOT NULL,
-    increment_by BIGINT NOT NULL,
-    max_value BIGINT NOT NULL,
-    min_value BIGINT NOT NULL,
-    cache_value BIGINT NOT NULL,
-    log_cnt BIGINT NOT NULL,
-    is_cycled BOOL NOT NULL,
-    is_called BOOL NOT NULL
-);
+
 CREATE TABLE queries
 (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     image_id INT,
-    extractor_id INT,
     extraction_id INT NOT NULL,
     metric_evaluator_id INT NOT NULL,
     query_duration BIGINT NOT NULL
 );
-CREATE TABLE queries_id_seq
-(
-    sequence_name VARCHAR NOT NULL,
-    last_value BIGINT NOT NULL,
-    start_value BIGINT NOT NULL,
-    increment_by BIGINT NOT NULL,
-    max_value BIGINT NOT NULL,
-    min_value BIGINT NOT NULL,
-    cache_value BIGINT NOT NULL,
-    log_cnt BIGINT NOT NULL,
-    is_cycled BOOL NOT NULL,
-    is_called BOOL NOT NULL
-);
+
 CREATE TABLE query_results
 (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     image_id INT,
     query_id INT,
     return_order INT,
     distance REAL NOT NULL
 );
-CREATE TABLE query_results_id_seq
-(
-    sequence_name VARCHAR NOT NULL,
-    last_value BIGINT NOT NULL,
-    start_value BIGINT NOT NULL,
-    increment_by BIGINT NOT NULL,
-    max_value BIGINT NOT NULL,
-    min_value BIGINT NOT NULL,
-    cache_value BIGINT NOT NULL,
-    log_cnt BIGINT NOT NULL,
-    is_cycled BOOL NOT NULL,
-    is_called BOOL NOT NULL
-);
+
 CREATE UNIQUE INDEX class_image_name_key ON class_image ( name );
 ALTER TABLE dataset_classes ADD FOREIGN KEY ( class_id ) REFERENCES class_image ( id ) ON UPDATE CASCADE;
 ALTER TABLE dataset_classes ADD FOREIGN KEY ( dataset_id ) REFERENCES datasets ( id ) ON UPDATE CASCADE;

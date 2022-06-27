a4734d8c2335a1b69bfffa5b90498a922f428a06
Guilherme Henrique <gjhenrique@gmail.com>
2022-06-27 12:53:51 +0200

    Unify migration script into the creation one

--- sql/schema-creation.sql
+++ sql/schema-creation.sql
@@ -30,7 +30,7 @@ CREATE TABLE extractors
 (
     id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(50),
-    filter_identifier VARCHAR(50),
+    type_identifier VARCHAR(50),
     class_name VARCHAR(50),
     levels_wavelet INT
 );
@@ -44,7 +44,7 @@ CREATE TABLE images
     mime_type VARCHAR(60)
 );
 
-CREATE TABLE metric_evaluator
+CREATE TABLE distance_functions
 (
     id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
@@ -56,17 +56,18 @@ CREATE TABLE queries
     id SERIAL PRIMARY KEY NOT NULL,
     image_id INT,
     extraction_id INT NOT NULL,
-    metric_evaluator_id INT NOT NULL,
-    query_duration BIGINT NOT NULL
+    distance_function_id INT NOT NULL,
+    query_duration BIGINT,
+    k INT
 );
 
 CREATE TABLE query_results
 (
     id SERIAL PRIMARY KEY NOT NULL,
-    image_id INT,
     query_id INT,
     return_order INT,
-    distance REAL NOT NULL
+    distance REAL NOT NULL,
+    extraction_id INT
 );
 
 CREATE UNIQUE INDEX class_image_name_key ON class_image ( name );
@@ -79,9 +80,10 @@ ALTER TABLE extractions ADD FOREIGN KEY ( image_id ) REFERENCES images ( id ) ON
 CREATE UNIQUE INDEX extractions_image_id_extractor_id_key ON extractions ( image_id, extractor_id );
 ALTER TABLE images ADD FOREIGN KEY ( dataset_class_id ) REFERENCES dataset_classes ( id ) ON UPDATE CASCADE;
 CREATE UNIQUE INDEX images_file_name_dataset_class_id_key ON images ( file_name, dataset_class_id );
-CREATE UNIQUE INDEX unique_name ON metric_evaluator ( name );
+CREATE UNIQUE INDEX unique_name ON distance_functions ( name );
 ALTER TABLE queries ADD FOREIGN KEY ( extraction_id ) REFERENCES extractions ( id );
 ALTER TABLE queries ADD FOREIGN KEY ( image_id ) REFERENCES images ( id ) ON UPDATE CASCADE;
-ALTER TABLE queries ADD FOREIGN KEY ( metric_evaluator_id ) REFERENCES metric_evaluator ( id );
-ALTER TABLE query_results ADD FOREIGN KEY ( image_id ) REFERENCES images ( id ) ON UPDATE CASCADE;
+ALTER TABLE queries ADD FOREIGN KEY ( distance_function_id ) REFERENCES distance_functions ( id );
+CREATE UNIQUE INDEX unique_queries_extraction_id_distance_function_id_k ON queries (extraction_id, distance_function_id, k);
 ALTER TABLE query_results ADD FOREIGN KEY ( query_id ) REFERENCES queries ( id ) ON UPDATE CASCADE;
+ALTER TABLE query_results ADD FOREIGN KEY ( extraction_id ) REFERENCES extractions ( id );
